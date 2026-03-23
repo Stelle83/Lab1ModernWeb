af952e905226c4ddd77ce16c95560466e1c0ec5e
Stelle83 <stelle83@gmail.com>
2026-03-23 21:34:56 +0100

    update BookController and BookService

--- src/main/java/org/example/lab1modernweb/books/BookController.java
+++ src/main/java/org/example/lab1modernweb/books/BookController.java
@@ -1,10 +1,13 @@
 package org.example.lab1modernweb.books;
 
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
@@ -27,43 +30,90 @@ public class BookController {
     }
 
     @GetMapping
-    public List<Book> getBooks() {
-        log.info("Real service class is: {} ", this.bookService.getClass().getName());
-        return bookService.getAllBooks();
+    public String listBooks(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "book/list";
     }
 
-    @PostMapping
-    public Book createBook(@RequestParam String title) {
-        return bookService.saveBook(title);
+    @GetMapping("/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("book", new CreateBookDTO());
+        return "book/create";
     }
 
-    @GetMapping("/update/{id}")
-    public void updateBookTitle(@PathVariable long id, @RequestParam String title) {
-        bookService.updateTitle(id, title);
+    @PostMapping("/create")
+    public String createBook(@Valid @ModelAttribute("book") CreateBookDTO dto,
+                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "book/create";
+        }
+
+        bookService.create(dto);
+        return "redirect:/books";
     }
 
-    @GetMapping("/page")
-    public Page<Book> booksPaged(Pageable pageable) {
-        return bookService.findBooks(pageable);
+    @GetMapping("/{id}/edit")
+    public String showUpdateForm(@PathVariable Long id, Model model) {
+        model.addAttribute("book", bookService.findUpdateDTOById(id));
+        return "book/update";
     }
 
-    @GetMapping("/books")
-    public Page<Book> booksPagedWithDefaults(
-            @PageableDefault(size = 2, sort = "title") Pageable pageable) {
-        return bookService.findBooks(pageable);
+    @PostMapping("/{id}/edit")
+    public String updateBook(@PathVariable Long id,
+                             @Valid @ModelAttribute("book") UpdateBookDTO dto,
+                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "book/update";
+        }
+
+        bookService.update(id, dto);
+        return "redirect:/books";
     }
 
-    /**
-     * Demonstrate transaction rollback.
-     * URL: /books/bulk-update?ids=1,2&newTitle=Updated&fail=true
-     */
-    @PutMapping("/bulk-update")
-    public String bulkUpdate(
-            @RequestParam List<Long> ids,
-            @RequestParam String newTitle,
-            @RequestParam(defaultValue = "false") boolean fail) {
-        bookService.updateTitlesInTransaction(ids, newTitle, fail);
-        return "Bulk update successful!";
+    @PostMapping("/{id}/delete")
+    public String deleteBook(@PathVariable Long id) {
+        bookService.delete(id);
+        return "redirect:/books";
     }
+
+//    @GetMapping
+//    public List<Book> getBooks() {
+//        log.info("Real service class is: {} ", this.bookService.getClass().getName());
+//        return bookService.getAllBooks();
+//    }
+//
+//    @PostMapping
+//    public Book createBook(@RequestParam String title) {
+//        return bookService.saveBook(title);
+//    }
+//
+//    @GetMapping("/update/{id}")
+//    public void updateBookTitle(@PathVariable long id, @RequestParam String title) {
+//        bookService.updateTitle(id, title);
+//    }
+//
+//    @GetMapping("/page")
+//    public Page<Book> booksPaged(Pageable pageable) {
+//        return bookService.findBooks(pageable);
+//    }
+//
+//    @GetMapping("/books")
+//    public Page<Book> booksPagedWithDefaults(
+//            @PageableDefault(size = 2, sort = "title") Pageable pageable) {
+//        return bookService.findBooks(pageable);
+//    }
+//
+//    /**
+//     * Demonstrate transaction rollback.
+//     * URL: /books/bulk-update?ids=1,2&newTitle=Updated&fail=true
+//     */
+//    @PutMapping("/bulk-update")
+//    public String bulkUpdate(
+//            @RequestParam List<Long> ids,
+//            @RequestParam String newTitle,
+//            @RequestParam(defaultValue = "false") boolean fail) {
+//        bookService.updateTitlesInTransaction(ids, newTitle, fail);
+//        return "Bulk update successful!";
+//    }
 
 }
--- src/main/java/org/example/lab1modernweb/books/BookService.java
+++ src/main/java/org/example/lab1modernweb/books/BookService.java
@@ -12,49 +12,108 @@ import java.util.List;
 @Service
 public class BookService {
 
-    private static final Logger log = LoggerFactory.getLogger(BookService.class);
+    //private static final Logger log = LoggerFactory.getLogger(BookService.class);
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper = new BookMapper();
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
+    public List<BookDTO> findAll() {
+        return bookRepository.findAll()
+                .stream()
+                .map(bookMapper::toDTO)
+                .toList();
     }
 
-    @Transactional
-    public Book saveBook(String title) {
-        log.info("Save book with title {}", title);
-        Book book = new Book();
-        book.setTitle(title);
-        return bookRepository.save(book);
+    public BookDTO findById(Long id) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException("Book with id " + id + " not found"));
+        return bookMapper.toDTO(book);
     }
 
-    @Transactional
-    public void updateTitlesInTransaction(List<Long> ids, String newTitle, boolean shouldFail) {
-        for (Long id : ids) {
-            bookRepository.findById(id).ifPresent(book -> {
-                book.setTitle(newTitle);
-                bookRepository.save(book);
-                log.info("Updated book ID: {} to title: {}", id, newTitle);
-            });
+    public void create(CreateBookDTO dto) {
+        Book book = bookMapper.toEntity(dto);
+        bookRepository.save(book);
+    }
+
+    public UpdateBookDTO findUpdateDTOById(Long id) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException("Book with id " + id + " not found"));
+
+        UpdateBookDTO dto = new UpdateBookDTO();
+        dto.setId(book.getId());
+        dto.setTitle(book.getTitle());
+        dto.setAuthor(book.getAuthor());
+        dto.setGenre(book.getGenre());
+        dto.setPublishedDate(book.getPublishedDate());
+
+        return dto;
+    }
+
+    public void update(Long id, UpdateBookDTO dto) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException("Book with id " + id + " not found"));
+
+        bookMapper.updateEntity(dto, book);
+        bookRepository.save(book);
+    }
+
+    public void delete(Long id) {
+        if (!bookRepository.existsById(id)) {
+            throw new BookNotFoundException("Book with id " + id + " not found");
         }
-
-        if (shouldFail) {
-        log.error("Intentional failure triggered for transaction rollback demo!");
-        throw new BulkUpdateException("Rollback requested!");
-    }
-}
-
-    @Transactional
-    public void updateTitle(long id, String title) {
-        bookRepository.updateTitle(id, title);
+        bookRepository.deleteById(id);
     }
 
-    @Transactional
-    public Page<Book> findBooks(Pageable pageable) {
-        return bookRepository.findAllBy(pageable);
-    }
+//    public BookService(BookRepository bookRepository) {
+//        this.bookRepository = bookRepository;
+//    }
+//
+//    public List<Book> findAll() {
+//        return bookRepository.findAll()
+//                .stream()
+//                .map(bookMapper::toDTO);
+//    }
+//
+//    public void create(CreateBookDTO dto) {
+//        Book book = bookMapper.toEntity(dto);
+//        bookRepository.save(book);
+//    }
+//
+//    @Transactional
+//    public Book saveBook(String title) {
+//        log.info("Save book with title {}", title);
+//        Book book = new Book();
+//        book.setTitle(title);
+//        return bookRepository.save(book);
+//    }
+//
+//    @Transactional
+//    public void updateTitlesInTransaction(List<Long> ids, String newTitle, boolean shouldFail) {
+//        for (Long id : ids) {
+//            bookRepository.findById(id).ifPresent(book -> {
+//                book.setTitle(newTitle);
+//                bookRepository.save(book);
+//                log.info("Updated book ID: {} to title: {}", id, newTitle);
+//            });
+//        }
+//
+//        if (shouldFail) {
+//        log.error("Intentional failure triggered for transaction rollback demo!");
+//        throw new BulkUpdateException("Rollback requested!");
+//    }
+//}
+//
+//    @Transactional
+//    public void updateTitle(long id, String title) {
+//        bookRepository.updateTitle(id, title);
+//    }
+//
+//    @Transactional
+//    public Page<Book> findBooks(Pageable pageable) {
+//        return bookRepository.findAllBy(pageable);
+//    }
 
 }
