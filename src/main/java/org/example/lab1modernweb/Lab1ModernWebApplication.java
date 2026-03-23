50a4d7ae16620b8b365b18e1cf839a449c09ca0a
Stelle83 <stelle83@gmail.com>
2026-03-23 12:13:08 +0100

    initial commit

---
+++ src/main/java/org/example/lab1modernweb/Lab1ModernWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.lab1modernweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1ModernWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1ModernWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lab1modernweb/Lab1ModernWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab1modernweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1ModernWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
